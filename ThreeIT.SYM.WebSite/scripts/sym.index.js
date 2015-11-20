eb5bf105c35c0651c4bd73b9a956826db596dcc3
marcelkumano <marcelkumano@hotmail.com>
2015-11-19 23:59:58 -0200

    prototipo

--- ThreeIT.SYM.WebSite/scripts/sym.index.js
+++ ThreeIT.SYM.WebSite/scripts/sym.index.js
@@ -1,14 +1,26 @@
 ﻿angular.module('sym.pesquisa-salas', ['ngAnimate', 'ui.bootstrap']);
 
-angular.module('sym.pesquisa-salas').controller('sym.pesquisa-salas.navbar', function ($scope, $log) {
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
 
-    $scope.navbarCollapsed = true;
+angular.module('sym.pesquisa-salas').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
 
+    $scope.items = items;
+    $scope.selected = {
+        item: $scope.items[0]
+    };
+
+    $scope.ok = function () {
+        $uibModalInstance.close($scope.selected.item);
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
 });
+    
 
-angular.module('sym.pesquisa-salas').controller('sym.pesquisa-salas.resultado', function ($scope, $log) {
-
- 
+angular.module('sym.pesquisa-salas').controller('sym.pesquisa-salas.resultado', function ($scope, $uibModal, $log) {
 
     $scope.items = [{
         mes: "novembro", descricaoMes: "Novembro 2015", dias: [
@@ -28,5 +40,26 @@ angular.module('sym.pesquisa-salas').controller('sym.pesquisa-salas.resultado', 
             { numeroDia: 12, diaSemana: 'quinta-feira', horarios: [{ hora: "7:00" }, { hora: "8:00" }, { hora: "9:00" }, { hora: "10:00" }, { hora: "11:00" }] }
         ]
     }];
+
+    $scope.open = function (size) {
+
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'modalAgendamento.html',
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            resolve: {
+                items: function () {
+                    return  ['item1', 'item2', 'item3'];
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
 
 });
