e6133c1e0f207e76432aa7db0004ed22ef38731f
marcelkumano <marcelkumano@hotmail.com>
2015-11-21 10:58:17 -0200

    pesquisa agendamento

--- ThreeIT.SYM.WebSite/scripts/sym.resultado-pesquisa-agendamento.js
+++ ThreeIT.SYM.WebSite/scripts/sym.resultado-pesquisa-agendamento.js
@@ -23,23 +23,91 @@ angular.module('sym.pesquisa-salas').controller('ModalInstanceCtrl', function ($
 angular.module('sym.pesquisa-salas').controller('sym.pesquisa-salas.resultado', function ($scope, $uibModal, $log) {
 
     $scope.items = [{
-        mes: "novembro", descricaoMes: "Novembro 2015", dias: [
-            { numeroDia: 11, diaSemana: 'quarta-feira', horarios: [{ hora: "7:00" }, { hora: "8:00" }, { hora: "9:00" }, { hora: "10:00" }, { hora: "11:00" }] },
-            { numeroDia: 12, diaSemana: 'quinta-feira', horarios: [{ hora: "7:00" }, { hora: "8:00" }, { hora: "9:00" }, { hora: "10:00" }, { hora: "11:00" }] }
-        ]
-    },
-    {
-        mes: "dezembro", descricaoMes: "Dezembro 2015", dias: [
-            { numeroDia: 11, diaSemana: 'quarta-feira', horarios: [{ hora: "7:00" }, { hora: "8:00" }, { hora: "9:00" }, { hora: "10:00" }, { hora: "11:00" }] },
-            { numeroDia: 12, diaSemana: 'quinta-feira', horarios: [{ hora: "7:00" }, { hora: "8:00" }, { hora: "9:00" }, { hora: "10:00" }, { hora: "11:00" }] }
-        ]
-    },
-    {
-        mes: "janeiro", descricaoMes: "Janeiro 2016", dias: [
-            { numeroDia: 11, diaSemana: 'quarta-feira', horarios: [{ hora: "7:00" }, { hora: "8:00" }, { hora: "9:00" }, { hora: "10:00" }, { hora: "11:00" }] },
-            { numeroDia: 12, diaSemana: 'quinta-feira', horarios: [{ hora: "7:00" }, { hora: "8:00" }, { hora: "9:00" }, { hora: "10:00" }, { hora: "11:00" }] }
-        ]
+        numeroMes: 11,
+        mes: "novembro",
+        descricaoMes: "Novembro 2015",
+        dias: [{
+            numeroDia: 11,
+            diaSemana: 'quarta-feira',
+            horarios: [{
+                hora: "7:00",
+                salas: [{
+                    primeiroHorario: 'indisponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }]
+            }, {
+                hora: "8:00",
+                salas: [{
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }]
+            }, {
+                hora: "9:00",
+                salas: [{
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }]
+            }, {
+                hora: "10:00",
+                salas: [{
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }]
+            }, {
+                hora: "11:00",
+                salas: [{
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }, {
+                    primeiroHorario: 'disponivel',
+                    segundoHorario: 'disponivel'
+                }]
+            }]
+        }]
     }];
+
 
     $scope.open = function (size) {
 
