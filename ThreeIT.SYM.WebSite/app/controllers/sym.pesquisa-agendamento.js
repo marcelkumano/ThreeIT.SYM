9715fa00d4b8d7938bd5f1e39b89123be16a669d
Rafa <rafael.marchetii@hotmail.com>
2015-11-23 15:23:50 -0200

    Filtro JSng - Sprint 2

--- ThreeIT.SYM.WebSite/app/controllers/sym.pesquisa-agendamento.js
+++ ThreeIT.SYM.WebSite/app/controllers/sym.pesquisa-agendamento.js
@@ -21,57 +21,15 @@ angular.module('sym.pesquisa-agendamento').controller('sym.pesquisa-agendamento.
 
 angular.module('sym.pesquisa-agendamento').controller('sym.pesquisa-agendamento.lista-local',
     function ($scope, $window, $http) {
-        $http.get('service/pesquisa-local').success(function(data){
-            $scope.lista = data;
-        });
+        //$http.get('http://localhost/sym.pesquisa-agendamento').success(function(data){
+        //    $scope.lista = data;
+        //});
 
-        $scope.unidade = { id: 0, local: '' };
-        
+        $scope.lista = [{ id:0, local:'Consolação' },
+                          { id:1, local:'Major Quedinho' },
+                          { id:2, local:'JB' },
+                          { id:3, local:'Canadá' }];
+        //$scope.lista = [];
     }
 );
 
-
-
-angular.module('sym.pesquisa-agendamento').controller('sym.pesquisa-salas.resultado', function ($scope, $uibModal, $log) {
-
-    $scope.items = [{
-        mes: "novembro", descricaoMes: "Novembro 2015", dias: [
-            { numeroDia: 11, diaSemana: 'quarta-feira', horarios: [{ hora: "7:00" }, { hora: "8:00" }, { hora: "9:00" }, { hora: "10:00" }, { hora: "11:00" }] },
-            { numeroDia: 12, diaSemana: 'quinta-feira', horarios: [{ hora: "7:00" }, { hora: "8:00" }, { hora: "9:00" }, { hora: "10:00" }, { hora: "11:00" }] }
-        ]
-    },
-    {
-        mes: "dezembro", descricaoMes: "Dezembro 2015", dias: [
-            { numeroDia: 11, diaSemana: 'quarta-feira', horarios: [{ hora: "7:00" }, { hora: "8:00" }, { hora: "9:00" }, { hora: "10:00" }, { hora: "11:00" }] },
-            { numeroDia: 12, diaSemana: 'quinta-feira', horarios: [{ hora: "7:00" }, { hora: "8:00" }, { hora: "9:00" }, { hora: "10:00" }, { hora: "11:00" }] }
-        ]
-    },
-    {
-        mes: "janeiro", descricaoMes: "Janeiro 2016", dias: [
-            { numeroDia: 11, diaSemana: 'quarta-feira', horarios: [{ hora: "7:00" }, { hora: "8:00" }, { hora: "9:00" }, { hora: "10:00" }, { hora: "11:00" }] },
-            { numeroDia: 12, diaSemana: 'quinta-feira', horarios: [{ hora: "7:00" }, { hora: "8:00" }, { hora: "9:00" }, { hora: "10:00" }, { hora: "11:00" }] }
-        ]
-    }];
-
-    $scope.open = function (size) {
-
-        var modalInstance = $uibModal.open({
-            animation: true,
-            templateUrl: 'modal-agendamento.html',
-            controller: 'ModalInstanceCtrl',
-            size: size,
-            resolve: {
-                items: function () {
-                    return ['item1', 'item2', 'item3'];
-                }
-            }
-        });
-
-        modalInstance.result.then(function (selectedItem) {
-            $scope.selected = selectedItem;
-        }, function () {
-            $log.info('Modal dismissed at: ' + new Date());
-        });
-    };
-
-});
