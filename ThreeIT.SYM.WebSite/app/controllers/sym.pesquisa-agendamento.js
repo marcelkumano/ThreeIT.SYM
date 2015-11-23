d39c8abee3d2e100e4ea10f9ddf29389b1c05ba7
Rafa <rafael.marchetii@hotmail.com>
2015-11-23 13:14:08 -0200

    Filtro JSng - Sprint 2

--- ThreeIT.SYM.WebSite/app/controllers/sym.pesquisa-agendamento.js
+++ ThreeIT.SYM.WebSite/app/controllers/sym.pesquisa-agendamento.js
@@ -1,7 +1,38 @@
-﻿angular.module('sym.pesquisa-local', ['ngAnimate', 'ui.bootstrap']);
+﻿angular.module('sym.pesquisa-agendamento', ['ngAnimate', 'ui.bootstrap']);
+
+angular.module('sym.pesquisa-agendamento').controller('sym.pesquisa-agendamento.contador-pessoas', 
+    function ($scope, $timeout) {
+        
+        $scope.contador = 0;
+
+        //Aumenta pessoas na sala
+        $scope.aumentar = function () {
+            $timeout($scope.contador++,1000);
+        };
+
+        //Subtrai pessoas na sala
+        $scope.diminuir = function ()
+        {
+            $timeout($scope.contador--, 1000);
+        }
+    }
+);
 
 
-angular.module('sym.pesquisa-local').controller('sym.pesquisa-salas.resultado', function ($scope, $uibModal, $log) {
+angular.module('sym.pesquisa-agendamento').controller('sym.pesquisa-agendamento.lista-local',
+    function ($scope, $window, $http) {
+        $http.get('service/pesquisa-local').success(function(data){
+            $scope.lista = data;
+        });
+
+        $scope.unidade = { id: 0, local: '' };
+        
+    }
+);
+
+
+
+angular.module('sym.pesquisa-agendamento').controller('sym.pesquisa-salas.resultado', function ($scope, $uibModal, $log) {
 
     $scope.items = [{
         mes: "novembro", descricaoMes: "Novembro 2015", dias: [
