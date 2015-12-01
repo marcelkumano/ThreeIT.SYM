74d88896b98e74c51086c291154194589646b230
VitorAlbani <vitoralbani@gmail.com>
2015-12-01 11:24:10 -0200

    Agendamento Inclusao

---
+++ ThreeIT.SYM.WebSite/app/controllers/_sym.agendamento.inclusao.js
@@ -0,0 +1,14 @@
+﻿/* Controllers */
+
+agendamentoControllers.controller('sym.agendamento.inclusao', function ($scope, $uibModalInstance, param) {
+
+    $scope.item = param;
+
+    $scope.ok = function () {
+        $uibModalInstance.close('ok');
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
