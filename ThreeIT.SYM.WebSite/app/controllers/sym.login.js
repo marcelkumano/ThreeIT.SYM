ea8d170bec51f6e546f5c0545f31019c51fa14ef
Rafa <rafael.marchetii@hotmail.com>
2015-12-05 11:41:08 -0200

    Login Fake - Sprint 3

--- ThreeIT.SYM.WebSite/app/controllers/sym.login.js
+++ ThreeIT.SYM.WebSite/app/controllers/sym.login.js
@@ -3,9 +3,19 @@
 agendamentoControllers.controller('sym.login',
     function ($scope, $timeout, $http, $location, appGlobalData) {
 
+        $scope.wrongUserPass = false;
+
         $scope.login = function (){
 
-            $location.path('filtro');
+            var usuario = $scope.usuario;
+            var senha = $scope.senha;
+            if (usuario == 'PADRAO' && senha == '123') {
+                $location.path('filtro');
+            }
+            else {
+                $scope.wrongUserPass = true;
+            }
+            
         }
     }
 );
